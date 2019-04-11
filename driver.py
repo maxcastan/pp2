9f7b30ad35a1f176bbba41e5d6955f7d11f8caeb
Max Castaneda <maxcastanedaiv@gmail.com>
2019-04-11 14:05:48 -0400

    Read input

--- driver.py
+++ driver.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LinearRegression
 'exec(%matplotlib inline)'
 
 class Tickers:
-    def save_tickers(n):
+    def save_tickers(self, n):
         '''
         Args:
             n: number of tickers to print to file
@@ -33,14 +33,14 @@ class Tickers:
             total_ticks.append(x[-5:])      #removes tabs
         for x in total_ticks:
             sys.stdout = open(os.devnull,"w")   #suppresses stdout
-            if(check_if_valid(x.strip())):
+            if(check_if_valid(self, x.strip())):
                 print(x.strip(),file =fopen)        #prints list of tickers to file tickers.txt
             else:
                 continue
             sys.stdout = sys.__stdout__
         fopen.close()
 
-    def check_if_valid(ticker):
+    def check_if_valid(self, ticker):
         '''
         uses iex-api-python to check if ticker has valid price function
         Args:
@@ -55,60 +55,15 @@ class Tickers:
         except(Exception,TypeError):
             return False
 
-class Fetcher:
 
-    def addSecs(tm, secs):
-        '''
-            Args:
-             tm: present times
-                secs: how many second to run
-         '''
-        date=datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
-        date=date+datetime.timedelta(seconds=secs)
-        return date.time()
-
-    def update_stock_info(ticker, stop):
-        '''
-            Args:
-                ticker: passed in single tickers
-                stop: return value from addSecs
-        '''
-        now=datetime.datetime.now()
-
-        if(now.time()>stop):
-            sys.exit()
-        sys.stdout = open(os.devnull,"w")
-        print(now.strftime("%H:%M"), file=outfile, end=',')
-        print(ticker,file = outfile,end =',')
-        print(Stock(ticker).quote().get('latestPrice'),file =outfile,end=',')
-        print(Stock(ticker).quote().get('latestVolume'),file =outfile,end=',')
-        print(Stock(ticker).quote().get('close'),file =outfile,end=',')
-        print(Stock(ticker).quote().get('open'),file =outfile,end=',')
-        print(Stock(ticker).quote().get('low'),file =outfile,end=',')
-        print(Stock(ticker).quote().get('high'),file =outfile)
-        sys.stdout = sys.__stdout__
-
-class Query:
-
-    def print_query_data(infofile,ticker,time, verbose):
-        '''
-            Args:
-                infofile: info.csv file generated from fetcher.py
-                ticker: single ticker name
-                time: time to search for in info.csv file
-                verbose: whether or not to include time/file info 
-        '''
-        with open(infofile) as csvfile:
-            csvreader =  csv.reader(csvfile,delimiter=',')
-            for row in csvreader:
-                if (row[0]==time)and(row[1]==ticker):
-                    if(verbose=='True'):
-                        print("Time: "+time)
-                        print("File: "+infofile)
-                        print("Ticker: "+ ticker)
-                    print("Latest Price: "+row[2])
-                    print("Latest Volume: "+row[3])
-                    print("Close: "+row[4])
-                    print("Open: "+row[5])
-                    print("Low: "+row[6])
-                    print("High: "+row[7]+"\n\n")
+if __name__=="__main__":
+    x=sys.argv[1]
+    if(x=="Ticker"):
+        count=sys.argv[2]
+        print(count)
+    elif(x=="Fetcher"):
+        x="4"
+    elif(x=="Query"):
+        x="4"
+    else:
+        print("Indicate Flag")
