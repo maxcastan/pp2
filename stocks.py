099a4a431594e9d9167b0b6a3285c3293f66f021
Max Castaneda <maxcastanedaiv@gmail.com>
2019-04-15 17:08:00 -0400

    Made Tickers Class

--- stocks.py
+++ stocks.py
@@ -13,17 +13,20 @@ from sklearn.linear_model import LinearRegression
 'exec(%matplotlib inline)'
 
 class Tickers:
-    def save_tickers(self, n):
+    def __init__(self, ticker_count):
+        self.ticker_count=int(ticker_count)
+        self.ticker='x'
+    def save_tickers(self):
         '''
         Args:
             n: number of tickers to print to file
         '''
-        fopen=open(sys.argv[2],'w+') #open the file in append mode
+        fopen=open('tickers.txt','w+') #open the file in append mode
         page = requests.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=200') #saves all url info
         doc = lxml.html.fromstring(page.content)
         table = doc.xpath('//div[@class = "genTable thin"]')[0] #grabs first instance of genTable, which stores tickers
         unparsed_ticks = []
-        for i in range(0,n):
+        for i in range(0, self.ticker_count):
             h3 = table.xpath('.//h3')[i]    #grabs all h3 headers
             ticks = h3.xpath('.//a')        #grabs a headers under h3
             for tick in ticks:
@@ -33,21 +36,22 @@ class Tickers:
             total_ticks.append(x[-5:])      #removes tabs
         for x in total_ticks:
             sys.stdout = open(os.devnull,"w")   #suppresses stdout
-            if(check_if_valid(self, x.strip())):
+            self.ticker=x.strip()
+            if(self.check_if_valid()):
                 print(x.strip(),file =fopen)        #prints list of tickers to file tickers.txt
             else:
                 continue
             sys.stdout = sys.__stdout__
         fopen.close()
 
-    def check_if_valid(self, ticker):
+    def check_if_valid(self):
         '''
         uses iex-api-python to check if ticker has valid price function
         Args:
             ticker: fetched ticker from NASDAQ website
         '''
         try:
-            price = Stock(ticker).price()
+            price = Stock(self.ticker).price()
             if price:
                 return True
             else:
@@ -59,8 +63,8 @@ class Tickers:
 if __name__=="__main__":
     x=sys.argv[1]
     if(x=="Ticker"):
-        ticker_count=sys.argv[2]
-        print(ticker_count)
+        p=Tickers(sys.argv[2])
+        p.save_tickers()
     elif(x=="Fetcher"):
         ticker_count=sys.argv[2]
         time_limit=sys.argv[3]
